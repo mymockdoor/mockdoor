7e282239df8bd917227daf30cd66215e22c80e19
mymockdoor <ace90210ace@msn.com>
2022-12-23 12:02:07 +0000

    fix issue with response grid not updating correctly

--- MockDoor/Shared/Models/ServiceRequest/ServiceRequestDto.cs
+++ MockDoor/Shared/Models/ServiceRequest/ServiceRequestDto.cs
@@ -9,7 +9,7 @@ using MockDoor.Shared.Models.Response;
 
 namespace MockDoor.Shared.Models.ServiceRequest
 {
-    public class ServiceRequestDto : IValidatableObject
+    public class ServiceRequestDto : ICopyTo<ServiceRequestDto>, IValidatableObject
     {
         public int Id { get; set; }
       
@@ -46,6 +46,62 @@ namespace MockDoor.Shared.Models.ServiceRequest
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             return Enumerable.Empty<ValidationResult>();
+        }
+
+        public ServiceRequestDto CopyTo(ServiceRequestDto target)
+        {
+            target.Id = Id;
+            target.FromUrl = FromUrl;
+            target.ExactUrlMatch = ExactUrlMatch;
+            target.ExpectAuthHeader = ExpectAuthHeader;
+            target.MockBehaviour = MockBehaviour;
+            target.Enabled = Enabled;
+            target.RestType = RestType;
+            target.Ttl = Ttl;
+            target.FromBody = FromBody;
+            target.SimulateTime = SimulateTime;
+            target.CreatedUtc = CreatedUtc;
+            target.MicroserviceId = MicroserviceId;
+            
+            target.MockResponses = new List<MockResponseDto>();
+            foreach(var response in MockResponses)
+            {
+                target.MockResponses.Add(response.CopyTo(new MockResponseDto()));
+            }
+            
+            if(target.RequestHeaders == null && RequestHeaders != null && RequestHeaders.Count > 0)
+                throw new ArgumentNullException($"{nameof(ServiceRequestDto)} Headers: Cannot copy to a null target");
+
+            if (RequestHeaders != null && RequestHeaders.Count > 0)
+            {
+                if (target.RequestHeaders == null)
+                {
+                    target.RequestHeaders = RequestHeaders;
+                }
+                else
+                {
+                    target.RequestHeaders.Clear();
+                    target.RequestHeaders.AddRange(RequestHeaders);
+                }
+            }
+            
+            if(target.QueryParameters == null && QueryParameters != null && QueryParameters.Count > 0)
+                throw new ArgumentNullException($"{nameof(ServiceRequestDto)} Query Parameters: Cannot copy to a null target");
+
+            if (QueryParameters != null && QueryParameters.Count > 0)
+            {
+                if (target.QueryParameters == null)
+                {
+                    target.QueryParameters = QueryParameters;
+                }
+                else
+                {
+                    target.QueryParameters.Clear();
+                    target.QueryParameters.AddRange(QueryParameters);
+                }
+            }
+
+            return target;
         }
     }
 }
